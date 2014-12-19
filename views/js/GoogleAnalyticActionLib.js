f17bc188f165a81d31ee2063e4f9cd9714bf939c
kermes <kermes@gmail.com>
2014-12-19 12:15:50 +0100

    Update GoogleAnalyticActionLib.js

--- views/js/GoogleAnalyticActionLib.js
+++ views/js/GoogleAnalyticActionLib.js
@@ -115,7 +115,7 @@ var GoogleAnalyticEnhancedECommerce = {
 		//this.add(Product);
 
 		ga('ec:setAction', 'refund', {
-			'id': Order.Id // Transaction ID is required for partial refund.
+			'id': Order.id // Transaction ID is required for partial refund.
 		});
 		ga('send', 'event','Refund','refund',{'nonInteraction':1});
 	},
